b0ed841c599f0ff928afa90004666f0b7e88daf1
Edmond Ina <97767271+Sarerrdy@users.noreply.github.com>
2023-12-13 16:55:25 +0100

    Delete 0x03-python-data_structures/2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    list_size = len(my_list)
-    if idx < 0 or idx > list_size:
-        return my_list
-    else:
-        my_list[idx] = element
-        return (my_list)
